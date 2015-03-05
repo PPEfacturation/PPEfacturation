14dce9041b9b10dade63888bbb02a59cb9ff1b81
etudiant <etudiant@w7-b317-12-cs>
2015-03-05 14:17:11 +0100

    fichier html test pour la structure html creation de la page d'acueil

---
+++ PPEfacturation/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>Service Facturation maison des ligues</title>
+	</head>
+	<body>
+		<header>
+			<h1>Maison des ligues</h1>
+		</header>
+	
+	</body>
+</html>
