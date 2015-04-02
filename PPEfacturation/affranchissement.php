37ad392a3b08334d843712059deba463bbacfb50
SautronChristian <c.sautron91@gmail.com>
2015-04-02 17:26:47 +0200

    Ajustement css menu; Ajout dans menu.php des liens non-remplie; Ajout du fichier affranchissement.php;

---
+++ PPEfacturation/affranchissement.php
@@ -0,0 +1,7 @@
+<?php
+	include('template/header.php');
+	include('template/footer.php');
+	
+?>
+<h1>Saisie Affranchissement</h1>
+
--- PPEfacturation/template/menu.php
+++ PPEfacturation/template/menu.php
@@ -1,5 +1,8 @@
 <nav>
 			<a href="gestionLocaux.php">Gestion des salles</a>
 			<a href="generationRecap.php" >Géneration de recapitulatif</a>
+			<a href="">Géneration de recapitulatif pour la région</a>
 			<a href=""></a>
+			<a href=""></a>
+			<a href="affranchissement.php">Saisie affranchissement</a>
 </nav>
