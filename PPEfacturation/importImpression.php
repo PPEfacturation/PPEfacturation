9e49dc587b63fbfe317d03e10640f646634e80a6
SautronChristian <c.sautron91@gmail.com>
2015-05-22 15:39:05 +0200

    Correction importImpression.php

--- PPEfacturation/importImpression.php
+++ PPEfacturation/importImpression.php
@@ -6,7 +6,7 @@ include("function/function.php");
 ?>
 <h1>Importation des impressions</h1>
 
-<form method="get" action="importImpression.php" id="uploadImpression">
+<form method="post" action="importImpression.php" id="uploadImpression" enctype="multipart/form-data">
 	 <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
       <input type="file" name="monfichier" />
       <input type="submit" id="valider"  name="valider"/>
@@ -15,46 +15,49 @@ include("function/function.php");
 </form>
 <?php
 $bdd = connexion();
-if(isset($_GET['valider'])&& isset($_FILES['monfichier'])){
-	$fichier=basename($_FILES['monfichier']['name']);
-		
-	$repertoireDestination = 'C:\Users\etudiant\git\PPEfacturation\PPEfacturation';
-	if(move_uploaded_file($fichier,$repertoireDestination.$fichier)){
-		echo"L'upload est reussi";
+if(isset($_POST['valider'])&& isset($_FILES['monfichier'])){
+	//$fichier=basename($_FILES['monfichier']['name']);
+	$fichier = $_FILES['monfichier'];
+	//$repertoireDestination = "C:\Users\etudiant\git\PPEfacturation\PPEfacturation\\file\\";
+	$repertoireDestination = "file/";
+	$deplacement = move_uploaded_file($fichier['tmp_name'],$repertoireDestination.$fichier['name']);
+	if($deplacement != false){
+		echo "L upload est reussi";
 	}
 	else {
-		echo"L'upload à echoué";
+		echo"L upload Ã  echouÃ©";
 	}
-	if (($handle = fopen("../".$fichier, "r")) !== FALSE)
+	if (($handle = fopen($repertoireDestination.$fichier['name'], "r")) !== FALSE)
 	{
-		$flag = true;
+		
 		while (($data = fgetcsv($handle, 1000, "\t")) !== FALSE)
 		{
-			if($flag) { $flag = false; continue; }
-			$sql = "INSERT INTO mrbs_impressions (JobIndex, protocol, userName, hostAdress, hostName, pages(sheet)Printed, pages(side)Printed, startTime, endTime, interpreterDuration, paperType, paperSize, printerNumber, color)
+			
+			$sql = "INSERT INTO mrbs_impressions (jobIndex, protocol, userName, hostAdress, hostName, page(sheet)Printed, page(side)Printed, startTime, endTime, interpreterDuration, paperType, paperSize, printerNumber, color)
 									VALUES ( '".$data[0]."','".$data[1]."','".$data[2]."','".$data[3]."','".$data[4]."','".$data[5]."','".$data[6]."','".$data[7]."','".$data[8]."','".$data[9]."','".$data[10]."','".$data[11]."','".$data[12]."','".$data[13]."')";
 			$bdd->query($sql);
-		}
-		echo "Le fichier ".$fichier." Ã  Ã©tÃ© ajoutÃ© Ã  la base de donnÃ©es.\n";
+		
+		echo "Le fichier ".$fichier['name']." Ã  Ã©tÃ© ajoutÃ© Ã  la base de donnÃ©es.\n";
 		fclose($handle);
+		}
 	}
-	
 	//TODO continuer la fonction et la tester , faire le traintement du fichier .dat (commande fichier CSV)
 	
 	/*if (is_uploaded_file($_FILES["monfichier"]["tmp_name"])) {
 		if (rename($_FILES["monfichier"]["tmp_name"],
 				$repertoireDestination)) {
 			echo "Le fichier temporaire ".$_FILES["monfichier"]["tmp_name"].
-			" a été déplacé vers ".$repertoireDestination;
+			" a ï¿½tï¿½ dï¿½placï¿½ vers ".$repertoireDestination;
 		}
 		else{
-			echo "Le déplacement du fichier temporaire a échoué".
-					" vérifiez l'existence du répertoire ".$repertoireDestination;
+			echo "Le dï¿½placement du fichier temporaire a ï¿½chouï¿½".
+					" vï¿½rifiez l'existence du rï¿½pertoire ".$repertoireDestination;
 		}
 	}
 	else{
-		echo "Le fichier n'a pas été uploadé (trop gros ?)";
+		echo "Le fichier n'a pas ï¿½tï¿½ uploadï¿½ (trop gros ?)";
 	}*/
+	
 }
 
 ?>
